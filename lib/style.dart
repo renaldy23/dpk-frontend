c3e433c1baa60ecdc2c66a9aa5ce7c524b679f80
renaldy23 <renaldyramadhan567@gmail.com>
2022-03-09 07:23:46 +0700

    push initial

--- lib/style.dart
+++ lib/style.dart
@@ -1,27 +1,27 @@
-import 'package:flutter/material.dart';
-import 'package:nb_utils/nb_utils.dart';
+  import 'package:flutter/material.dart';
+  import 'package:nb_utils/nb_utils.dart';
 
-// Colors
-// const stylePrimaryColor = Color(0xFF3281FF);
-const styleAppBackground = Color(0xFFf8f8f8); //Color(0xFFE3F9FD);
-const styleShadowColor = Color(0x95E9EBF0);
-const styleTextSecondaryColor = Color(0xFF757575);
-const styleViewColor = Color(0xFFDADADA);
-const styleSecondaryColorRed = Color(0xFFFF5C5C);
-const styleIconColor = Color(0xFFA6A7AA);
+  // Colors
+  // const stylePrimaryColor = Color(0xFF3281FF);
+  const styleAppBackground = Color(0xFFf8f8f8); //Color(0xFFE3F9FD);
+  const styleShadowColor = Color(0x95E9EBF0);
+  const styleTextSecondaryColor = Color(0xFF757575);
+  const styleViewColor = Color(0xFFDADADA);
+  const styleSecondaryColorRed = Color(0xFFFF5C5C);
+  const styleIconColor = Color(0xFFA6A7AA);
 
-const stylePrimaryColor = Color(0xFF2697FF);
-const styleSecondaryColor = Color(0xFF2A2D3E);
-const styleBgColor = Color(0xFF212332);
+  const stylePrimaryColor = Color(0xFF2697FF);
+  const styleSecondaryColor = Color(0xFF2A2D3E);
+  const styleBgColor = Color(0xFF212332);
 
-const styleDefaultPadding = 16.0;
-const kDefaultPadding = 20.0;
-const kBgLightColor = Color(0xFFF2F4FC);
-const kTextColor = Color(0xFF4D5875);
-const kBgDarkColor = Color(0xFFEBEDFA);
-const kPrimaryColor = Color(0xFF366CF6);
-const kGrayColor = Color(0xFF8793B2);
-const kBadgeColor = Color(0xFFEE376E);
+  const styleDefaultPadding = 16.0;
+  const kDefaultPadding = 20.0;
+  const kBgLightColor = Color(0xFFF2F4FC);
+  const kTextColor = Color(0xFF4D5875);
+  const kBgDarkColor = Color(0xFFEBEDFA);
+  const kPrimaryColor = Color(0xFF366CF6);
+  const kGrayColor = Color(0xFF8793B2);
+  const kBadgeColor = Color(0xFFEE376E);
 
 // Widget
 BoxDecoration styleBoxDecoration(
